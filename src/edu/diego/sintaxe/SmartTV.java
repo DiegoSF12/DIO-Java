61779054994c5861c19cbbc2fbb4f556b1abc63c
Dieguim <diegofranciscodesouza7@gmail.com>
2024-06-03 17:39:51 -0300

    Metodos

---
+++ src/edu/diego/sintaxe/SmartTV.java
@@ -0,0 +1,7 @@
+package edu.diego.sintaxe;
+
+public class SmartTV {
+    boolean estado = false;
+    int canais = 0;
+    int volume = 0;
+}
---
+++ src/edu/diego/sintaxe/Usuario.java
@@ -0,0 +1,14 @@
+package edu.diego.sintaxe;
+
+public class Usuario {
+    public static void main(String[] args){
+        SmartTV novaTV = new SmartTV();
+
+        novaTV.estado = true;
+        novaTV.canais = 45;
+        novaTV.volume = 25;
+
+    }
+
+}
+
