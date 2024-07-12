ba0441575012c239a5c385b4ee4ff96e2ac9f5da
Dieguim <diegofranciscodesouza7@gmail.com>
2024-07-12 18:12:54 -0300

    Adição de pacote POO

---
+++ src/POO/Students.java
@@ -0,0 +1,25 @@
+package POO;
+
+public class Students{
+    private String name;
+    private int age;
+    private String color;
+    private String sex;
+
+    public Students(String name, int age, String color, String sex){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.sex = sex;
+    }
+
+    public void eatin(){
+        System.out.printf("%s eating\n", this.name);
+    }
+    public void running(){
+        System.out.printf("%s is running\n", this.name);
+    }
+    public void drinking(){
+        System.out.printf("%s drinking\n", this.name);
+    }
+}
---
+++ src/POO/StudentsTest.java
@@ -0,0 +1,11 @@
+package POO;
+
+public class StudentsTest {
+    public static void main(String[] args) {
+        Students student1 = new Students("Diogenis",20,"Light skin", "male");
+
+        student1.drinking();
+        student1.eatin();
+        student1.running();
+    }
+}
