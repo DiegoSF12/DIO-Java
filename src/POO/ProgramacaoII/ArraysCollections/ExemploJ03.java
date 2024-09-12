9db845a5fc9506e5417ecdbe4799c1b5f23ba937
Dieguim <diegofranciscodesouza7@gmail.com>
2024-09-12 18:37:17 -0300

    Adição de arquivos de visualização de arrays de forma gráfica

---
+++ src/POO/ProgramacaoII/ArraysCollections/ExemploJ03.java
@@ -0,0 +1,40 @@
+package POO.ProgramacaoII.ArraysCollections;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ExemploJ03 extends JFrame {
+    JTextArea saidaArea;
+
+    public ExemploJ03(){
+        saidaArea = new JTextArea();
+        Container container = getContentPane();
+        container.add(saidaArea);
+
+        int[][] array1 = {{1,2,3}, {4,5,6}};
+        int[][] array2 = {{1,2,3}, {4,5,6}};
+
+        saidaArea.setText("Os valores do array1 são: \n");
+        buildSaida(array1);
+
+        saidaArea.append("Os valores do array2 são: \n");
+        buildSaida(array2);
+
+        setSize(400,400);//CONFIGURANDO O TAMANHO DA JANELA
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//FECHA A APLICAÇÃO AO CLICAR NO BOTÃO FECHAR
+        setVisible(true);//TORNA A JANELA VISIVEL
+    }
+    public void buildSaida(int[][] array){
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++ ){
+                saidaArea.append(array[i][j] + "");
+            }
+            saidaArea.append("\n");
+        }
+        saidaArea.append("\n");
+    }
+    public static void main(String[] args){
+        new ExemploJ03();
+        System.out.println("Que trem fei");
+    }
+}
