ac30784aca761785aed08d8eb44a03a2903f81fe
Dieguim <Diegofranciscodesouza7@gmail.com>
2024-10-08 20:43:08 -0300

    Teste

---
+++ src/Testes/PushTEST.java
@@ -0,0 +1,9 @@
+package Testes;
+
+import javax.swing.JOptionPane;
+
+public class PushTEST {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Teste de push and pull");
+    }
+}
